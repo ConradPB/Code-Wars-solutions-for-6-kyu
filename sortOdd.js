bf9cdea100acaa2c8a1afc970ce3740d9a1b5781
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2022-09-27 22:57:50 +0300

    Sorting odd numbers

---
+++ sortOdd.js
@@ -0,0 +1,7 @@
+/* You are given an array of numbers. 
+
+Sort the odd numbers in ascending order while leaving the even numbers at their original positions. */
+
+function sortArray(array) {
+    
+  }
