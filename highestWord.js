a2e8e868c1bbd6dc06f14d9a03ce02702c351244
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2022-09-29 23:58:52 +0300

    Find the highest word

---
+++ highestWord.js
@@ -0,0 +1,22 @@
+/* 
+Given a string of words, you need to find the highest scoring word.
+
+Each letter of a word scores points according to its position in the alphabet: a = 1, b = 2, c = 3 etc.
+
+You need to return the highest scoring word as a string.
+
+If two words score the same, return the word that appears earliest in the original string.
+
+All letters will be lowercase and all inputs will be valid. 
+*/
+
+function high(x){
+    //We look for the word with the highest score
+    //Lets create 2 variables
+
+    let highestScore = 0
+    let highestWord = ''
+    
+
+    
+  }
