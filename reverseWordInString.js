9a41f1365c0387a78caf48efca29863c71ed5fdf
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2022-10-09 22:23:42 +0300

    (incomplete) reversing every over word

--- reverseWordInString.js
+++ reverseWordInString.js
@@ -5,6 +5,16 @@ Punctuation marks should be treated as if they are a part of the word in this ka
 */
 
 function reverse(str){
-    ourArray = string.split
-    ourArray.map.with_index{|word,index| index.odd? ? word.reverse : word }.join(' ')
+    let newStringArr = str.split(' ')
+
+    for ( let i=0; i<newStringArr.length; i++) {
+      newStringArr[i] = newStringArr[i].split('')
+      newStringArr[i].reverse()
+      newStringArr[i] = newStringArr[i].join('')
+    }
+    newStringArr = newStringArr.join(' ')
+    
+
+    return newStringArr
+    
   }
