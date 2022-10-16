e1e90dc891c555c7d8939b2470ef3c9968589568
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2022-10-16 23:59:52 +0300

    Replace with Alpha

--- ReplaceWithAlpha.js
+++ ReplaceWithAlpha.js
@@ -14,7 +14,9 @@ Should return "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 1
 
 function alphabetPosition(text) {
     if (typeof text !== "string") 
-    return "wrong entry";
+    return "wrong entry"
+    text = text.toLowerCase()
+    const result = ''
 
 
 }
