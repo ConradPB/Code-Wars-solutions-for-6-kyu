17cb9330f11a7e5ae1849a92b89ef718b4e7d907
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2022-10-17 23:59:43 +0300

    Replace with Alpha

--- ReplaceWithAlpha.js
+++ ReplaceWithAlpha.js
@@ -17,6 +17,35 @@ function alphabetPosition(text) {
     return "wrong entry"
     text = text.toLowerCase()
     const result = ''
+    const alphabets = {
+        a: "1 ",
+        b: "2 ",
+        c: "3 ",
+        d: "4 ",
+        e: "5 ",
+        f: "6 ",
+        g: "7 ",
+        h: "8 ",
+        i: "9 ",
+        j: "10 ",
+        k: "11 ",
+        l: "12 ",
+        m: "13 ",
+        n: "14 ",
+        o: "15 ",
+        p: "16 ",
+        q: "17 ",
+        r: "18 ",
+        s: "19 ",
+        t: "20 ",
+        u: "21 ",
+        v: "22 ",
+        w: "23 ",
+        x: "24 ",
+        y: "25 ",
+        z: "26 ",
+      };
+      for (let i = 0; i < text.length; ++i) {
 
 
 }
