2f301779223ade6829cd0b5519ac220a6da7ddc7
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2022-11-14 02:38:35 +0300

    Stop gninnipS My sdroW!

--- Stop.js
+++ Stop.js
@@ -1,4 +1,16 @@
 function spinWords(string){
     const stringArray = string.split(' ')
     let result = ''
+
+    sentenceArray.map((str, i) => {
+        if (str.length >= 5){
+          sentenceArray[i] = str.split('').reverse().join('');
+        } else {
+          sentenceArray[i] = str;
+        }
+      result = sentenceArray.join(' ');
+      })
+
+    
+    
 }
